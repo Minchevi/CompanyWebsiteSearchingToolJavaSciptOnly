c4aaabdf3bb7129abb78dd5b52cc2b2eaf0cb7df
asyamincheva <asya.m.mincheva@gmail.com>
2015-07-20 16:26:32 +0300

    get data from searchbox & find correct results

--- js/selectDataByInput.js
+++ js/selectDataByInput.js
@@ -1,3 +1,31 @@
-function selectDataByInput() {
-    //...
-}
+$(function () {
+    // Attach on 'keyup' for live update
+    //('change' fires when the focus is lost)
+    $("#tb-filter").on('keyup', function () {
+        var value,
+            $companies;
+        value = $(this).val().toLowerCase();
+        var data1 = data[0];
+        console.log(data1);
+        if (value.length > 2) {
+
+            //$regex = '%' + value + '%';
+            //console.log($regex);
+            data.forEach(function(company){
+                var companyName = company[1].toLowerCase();
+                console.log(companyName);
+                if(companyName.indexOf(value) > -1){
+                    // TODO: get data from the variables and send to displayData !
+                }
+            });
+
+        }
+    });
+
+    //$.ajax({
+//
+    //}).success(function (respCountries) {
+    //    var $countriesList = createCountriesList(respCountries);
+    //    $('#countries').html($countriesList);
+    //});
+});
