a473ff68093c6902dad3ba168a58d72908e7b6d6
Nelly Mincheva <nelly.m.mincheva@gmail.com>
2015-07-20 11:05:15 +0300

    script added in the html

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $.ajax({
         type: "GET",
-        url: "data.txt",
+        url: "base.csv",
         dataType: "text",
         success: function(data) {processData(data);}
     });
