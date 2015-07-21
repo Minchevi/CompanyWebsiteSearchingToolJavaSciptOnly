5c2a169ad9fd744c2f8b8c8b404d19f87b0b21db
Nelly Mincheva <nelly.m.mincheva@gmail.com>
2015-07-21 09:00:39 +0300

    link opened in new tab fixed

--- js/displayData.js
+++ js/displayData.js
@@ -2,5 +2,5 @@ function displayData(company) {
     var companyName = company[1];
     var companyLink = company[2];
 
-    $('#companies').append('<li class="company"><a href="'+companyLink+'">'+companyName+'</a></li>');
+    $('#companies').append('<li class="company"><a href="'+companyLink+'" target="_blank">'+companyName+'</a></li>');
 };
